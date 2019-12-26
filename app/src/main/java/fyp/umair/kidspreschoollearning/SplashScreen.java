b6841f2237ececad03fc0b034dbdabfd13b05e73
shahrukhktk <shahrukhktk760@gmail.com>
2019-12-26 13:11:43 +0500

    splash screen xml done

---
+++ app/src/main/java/fyp/umair/kidspreschoollearning/SplashScreen.java
@@ -0,0 +1,32 @@
+package fyp.umair.kidspreschoollearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable()
+        {
+            // Using handler with postDelayed called runnable run method
+            @Override
+            public void run() {
+
+                startActivity(new Intent(getApplicationContext(), SplashScreen.class));
+
+                // close this activity
+                finish();
+            }
+        }, 3*1000); // wait for 3 seconds
+
+    }
+}
