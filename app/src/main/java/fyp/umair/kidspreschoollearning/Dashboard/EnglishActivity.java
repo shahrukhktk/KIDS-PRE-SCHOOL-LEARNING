e620b6892df468adc229728d9a17ead9627053f5
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-02 19:56:09 +0500

    english activity xml completed

---
+++ app/src/main/java/fyp/umair/kidspreschoollearning/Dashboard/EnglishActivity.java
@@ -0,0 +1,14 @@
+package fyp.umair.kidspreschoollearning.Dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnglishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_english);
+    }
+}
