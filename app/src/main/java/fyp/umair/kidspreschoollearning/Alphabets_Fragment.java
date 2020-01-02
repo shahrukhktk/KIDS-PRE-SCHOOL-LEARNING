55fe8bb59b94ad3041c5e734b5a5489dae03d86a
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-02 20:48:36 +0500

    alphabets fragments xml starts

---
+++ app/src/main/java/fyp/umair/kidspreschoollearning/Alphabets_Fragment.java
@@ -0,0 +1,26 @@
+package fyp.umair.kidspreschoollearning;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Alphabets_Fragment extends Fragment {
+
+    public Alphabets_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alphabets_, container, false);
+    }
+
+}
