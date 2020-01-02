b39dbf567bd841f3b3c06de26cdddface4f9cd3e
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-02 21:22:04 +0500

    alphabets fragments xml done

--- app/src/main/java/fyp/umair/kidspreschoollearning/English_Category/Alphabets_Fragment.java
+++ app/src/main/java/fyp/umair/kidspreschoollearning/English_Category/Alphabets_Fragment.java
@@ -1,4 +1,4 @@
-package fyp.umair.kidspreschoollearning;
+package fyp.umair.kidspreschoollearning.English_Category;
 
 import android.content.Context;
 import android.net.Uri;
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import fyp.umair.kidspreschoollearning.R;
+
 public class Alphabets_Fragment extends Fragment {
 
     public Alphabets_Fragment() {
@@ -20,7 +22,8 @@ public class Alphabets_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alphabets_, container, false);
+        View view = inflater.inflate(R.layout.fragment_alphabets_, container, false);
+        return view;
     }
 
 }
--- app/src/main/java/fyp/umair/kidspreschoollearning/English_Category/EnglishAlphabets_Activity.java
+++ app/src/main/java/fyp/umair/kidspreschoollearning/English_Category/EnglishAlphabets_Activity.java
@@ -1,16 +1,32 @@
 package fyp.umair.kidspreschoollearning.English_Category;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 import fyp.umair.kidspreschoollearning.R;
 
 public class EnglishAlphabets_Activity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_english_alphabets_);
+    }
+
+    public void EnglishAlphabetsFragmentsCall(View view) {
+        if (view == findViewById(R.id.character_A_Id))
+        {
+            Fragment fragment = new Alphabets_Fragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_container_ID, fragment);
+            ft.commit();
+        }
     }
 }
