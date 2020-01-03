451a79033f797fe1332051449d37b50d9bc66b75
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-03 10:25:40 +0500

    ALL Fragments XML Completed

--- app/src/main/java/fyp/umair/kidspreschoollearning/English_Category/EnglishAlphabets_Activity.java
+++ app/src/main/java/fyp/umair/kidspreschoollearning/English_Category/EnglishAlphabets_Activity.java
@@ -72,168 +72,192 @@ public class EnglishAlphabets_Activity extends AppCompatActivity
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_C_Id))
+        }
+        else if(view == findViewById(R.id.character_C_Id))
         {
             Fragment fragment = new Character_C_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_D_Id))
+        }
+        else if(view == findViewById(R.id.character_D_Id))
         {
             Fragment fragment = new Character_D_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_E_Id))
+        }
+        else if(view == findViewById(R.id.character_E_Id))
         {
             Fragment fragment = new Character_E_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_F_Id))
+        }
+        else if(view == findViewById(R.id.character_F_Id))
         {
             Fragment fragment = new Character_F_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_G_Id))
+        }
+        else if(view == findViewById(R.id.character_G_Id))
         {
             Fragment fragment = new Character_G_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_H_Id))
+        }
+        else if(view == findViewById(R.id.character_H_Id))
         {
             Fragment fragment = new Character_H_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_I_Id))
+        }
+        else if(view == findViewById(R.id.character_I_Id))
         {
             Fragment fragment = new Character_I_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_J_Id))
+        }
+        else if(view == findViewById(R.id.character_J_Id))
         {
             Fragment fragment = new Character_J_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_K_Id))
+        }
+        else if(view == findViewById(R.id.character_K_Id))
         {
             Fragment fragment = new Character_K_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_L_Id))
+        }
+        else if(view == findViewById(R.id.character_L_Id))
         {
             Fragment fragment = new Character_L_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_M_Id))
+        }
+        else if(view == findViewById(R.id.character_M_Id))
         {
             Fragment fragment = new Character_M_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_N_Id))
+        }
+        else if(view == findViewById(R.id.character_N_Id))
         {
             Fragment fragment = new Character_N_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_O_Id))
+        }
+        else if(view == findViewById(R.id.character_O_Id))
         {
             Fragment fragment = new Character_O_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_P_Id))
+        }
+        else if(view == findViewById(R.id.character_P_Id))
         {
             Fragment fragment = new Character_P_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_Q_Id))
+        }
+        else if(view == findViewById(R.id.character_Q_Id))
         {
             Fragment fragment = new Character_Q_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_R_Id))
+        }
+        else if(view == findViewById(R.id.character_R_Id))
         {
             Fragment fragment = new Character_R_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_S_Id))
+        }
+        else if(view == findViewById(R.id.character_S_Id))
         {
             Fragment fragment = new Character_S_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_T_Id))
+        }
+        else if(view == findViewById(R.id.character_T_Id))
         {
             Fragment fragment = new Character_T_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_U_Id))
+        }
+        else if(view == findViewById(R.id.character_U_Id))
         {
             Fragment fragment = new Character_U_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_V_Id))
+        }
+        else if(view == findViewById(R.id.character_V_Id))
         {
             Fragment fragment = new Character_V_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_W_Id))
+        }
+        else if(view == findViewById(R.id.character_W_Id))
         {
             Fragment fragment = new Character_W_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_X_Id))
+        }
+        else if(view == findViewById(R.id.character_X_Id))
         {
             Fragment fragment = new Character_X_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_Y_Id))
+        }
+        else if(view == findViewById(R.id.character_Y_Id))
         {
             Fragment fragment = new Character_Y_Fragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.replace(R.id.fragment_container_ID, fragment);
             ft.commit();
-        }else if(view == findViewById(R.id.character_Z_Id))
+        }
+        else if(view == findViewById(R.id.character_Z_Id))
         {
             Fragment fragment = new Character_Z_Fragment();
             FragmentManager fm = getSupportFragmentManager();
--- app/src/main/java/fyp/umair/kidspreschoollearning/English_Category/TraceLetters_Activity.java
+++ app/src/main/java/fyp/umair/kidspreschoollearning/English_Category/TraceLetters_Activity.java
@@ -10,7 +10,8 @@ public class TraceLetters_Activity extends AppCompatActivity
 {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trace_letters_);
     }
