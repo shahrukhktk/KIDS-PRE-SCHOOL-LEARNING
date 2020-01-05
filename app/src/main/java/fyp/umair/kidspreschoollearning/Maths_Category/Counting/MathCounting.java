43a717ed8e56251a2547b882e148b1bb11f5d7e7
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-05 19:04:37 +0500

    Working on Counting XML

--- app/src/main/java/fyp/umair/kidspreschoollearning/Maths_Category/Counting/MathCounting.java
+++ app/src/main/java/fyp/umair/kidspreschoollearning/Maths_Category/Counting/MathCounting.java
@@ -2,8 +2,11 @@ package fyp.umair.kidspreschoollearning.Maths_Category.Counting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import fyp.umair.kidspreschoollearning.Maths_Category.MathsDashboard;
 import fyp.umair.kidspreschoollearning.R;
 
 public class MathCounting extends AppCompatActivity
@@ -14,5 +17,15 @@ public class MathCounting extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_counting);
+    }
+
+    public void LaunchMathDashboard(View view)
+    {
+        startActivity(new Intent(getApplicationContext(), MathsDashboard.class));
+    }
+
+    public void CoutingNumbersFragmentsCall(View view)
+    {
+
     }
 }
