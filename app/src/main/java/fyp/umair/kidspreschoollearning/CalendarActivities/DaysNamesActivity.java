8bd354af24f78bf83cc192105525e86804ed7b5d
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-25 15:44:58 +0500

    MOnths XML - Completed All fragments

---
+++ app/src/main/java/fyp/umair/kidspreschoollearning/CalendarActivities/DaysNamesActivity.java
@@ -0,0 +1,16 @@
+package fyp.umair.kidspreschoollearning.CalendarActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fyp.umair.kidspreschoollearning.R;
+
+public class DaysNamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_days_names);
+    }
+}
---
+++ app/src/main/java/fyp/umair/kidspreschoollearning/CalendarActivities/IslamicMonths.java
@@ -0,0 +1,16 @@
+package fyp.umair.kidspreschoollearning.CalendarActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fyp.umair.kidspreschoollearning.R;
+
+public class IslamicMonths extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_islamic_months);
+    }
+}
