493dc77ec681dbe7cac674671de649efe1c37e32
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-25 15:10:17 +0500

    Maths Addition and Subtraction XML - Completed All fragments

--- app/src/main/java/fyp/umair/kidspreschoollearning/Maths_Category/Subtraction/MathSubtraction.java
+++ app/src/main/java/fyp/umair/kidspreschoollearning/Maths_Category/Subtraction/MathSubtraction.java
@@ -3,6 +3,7 @@ package fyp.umair.kidspreschoollearning.Maths_Category.Subtraction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import fyp.umair.kidspreschoollearning.R;
 
@@ -12,5 +13,8 @@ public class MathSubtraction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_subtraction);
+    }
+
+    public void LaunchDashboard(View view) {
     }
 }
