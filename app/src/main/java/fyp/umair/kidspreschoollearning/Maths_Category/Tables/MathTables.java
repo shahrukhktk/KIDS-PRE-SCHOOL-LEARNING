f183a14ab2189ab6b8d810aa5857c92bd2b47c27
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-25 13:31:09 +0500

    Alphabets Activity XML - Completed All fragments

--- app/src/main/java/fyp/umair/kidspreschoollearning/Maths_Category/Tables/MathTables.java
+++ app/src/main/java/fyp/umair/kidspreschoollearning/Maths_Category/Tables/MathTables.java
@@ -29,38 +29,38 @@ public class MathTables extends AppCompatActivity {
     }
 
     public void TablesNumber2(View view) {
-        tableId.setImageResource(R.drawable.table2_img);
+        tableId.setImageResource(R.drawable.img_table_2);
     }
 
     public void TablesNumber3(View view) {
-        tableId.setImageResource(R.drawable.table3_img);
+        tableId.setImageResource(R.drawable.img_table_3);
     }
 
     public void TablesNumber4(View view) {
-        tableId.setImageResource(R.drawable.table4_img);
+        tableId.setImageResource(R.drawable.img_table_4);
     }
 
     public void TablesNumber5(View view) {
-        tableId.setImageResource(R.drawable.table5_img);
+        tableId.setImageResource(R.drawable.img_table_5);
     }
 
     public void TablesNumber6(View view) {
-        tableId.setImageResource(R.drawable.table6_img);
+        tableId.setImageResource(R.drawable.img_table_6);
     }
 
     public void TablesNumber7(View view) {
-        tableId.setImageResource(R.drawable.table7_img);
+        tableId.setImageResource(R.drawable.img_table_7);
     }
 
     public void TablesNumber8(View view) {
-        tableId.setImageResource(R.drawable.table8_img);
+        tableId.setImageResource(R.drawable.img_table_8);
     }
 
     public void TablesNumber9(View view) {
-        tableId.setImageResource(R.drawable.table9_img);
+        tableId.setImageResource(R.drawable.img_table_9);
     }
 
     public void TablesNumber10(View view) {
-        tableId.setImageResource(R.drawable.table10_img);
+        tableId.setImageResource(R.drawable.img_table_10);
     }
 }
